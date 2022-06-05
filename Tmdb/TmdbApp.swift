bcb94bfad69e28cadac18af010e72058ea12b089
mukul372 <mukul@scogo.in>
2022-06-05 09:56:45 +0530

    Initial Commit

---
+++ Tmdb/TmdbApp.swift
@@ -0,0 +1,17 @@
+//
+//  TmdbApp.swift
+//  Tmdb
+//
+//  Created by Mukul Jangir on 05/06/22.
+//
+
+import SwiftUI
+
+@main
+struct TmdbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
